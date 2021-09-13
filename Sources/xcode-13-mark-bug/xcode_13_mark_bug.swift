e59cdc55aef23f24d9b32c630f0413b2a4fc23eb
Benjamin Horsleben <benjamin@fizkerinc.dk>
2021-09-13 20:09:58 +0200

    Added code with bug

---
+++ Sources/xcode-13-mark-bug/xcode_13_mark_bug.swift
@@ -0,0 +1,30 @@
+struct xcode_13_mark_bug {
+	var text = "Hello, World!"
+}
+
+// MARK: - 1
+let foo = [
+	// MARK: First
+	1,
+	2,
+	3,
+	// MARK: Middle
+	4,
+	5,
+	6,
+	// MARK: Last
+]
+
+// MARK: - 2
+let bar =
+[
+	// MARK: First
+	1,
+	2,
+	3,
+	// MARK: Middle
+	4,
+	5,
+	6,
+	// MARK: Last
+]
